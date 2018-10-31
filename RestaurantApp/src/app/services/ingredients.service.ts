eabb0edc211862b49c67f627515612278bba9490
Chz216 <joseluis.romero@alumno.uttehuacan.edu.mx>
2018-10-30 18:30:21 -0600

    30/10/2018

--- RestaurantApp/src/app/services/ingredients.service.ts
+++ RestaurantApp/src/app/services/ingredients.service.ts
@@ -1,5 +1,5 @@
 import { Ingredient } from "../shared/ingredient.model";
-import { EventEmitter } from '@angular/core';
+//import { EventEmitter } from '@angular/core';
 import { Subject } from "../../../node_modules/rxjs";
 
 export class IngredientsService {
--- RestaurantApp/src/app/shopping-list/shopping-list.component.ts
+++ RestaurantApp/src/app/shopping-list/shopping-list.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model'
 import { IngredientsService } from '../services/ingredients.service';
+import { Subscription } from '../../../node_modules/rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients : Ingredient [];
+  private subscription: Subscription;
   constructor(private ingredientsService: IngredientsService) { }
 
   ngOnInit() {
@@ -19,6 +21,10 @@ export class ShoppingListComponent implements OnInit {
          this.ingredients = ingredients;
        })
      )
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   onEditItem(index: number){
